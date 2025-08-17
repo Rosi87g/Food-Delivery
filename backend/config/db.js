af416d65d6978b08d1c60ebf4f5c6a37cf030781
Rameez <roseham827@gmail.com>
2025-08-17 10:57:08 +0530

    Delete backend/config directory

--- backend/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://teamfav19CreateStack:9HN8mI8WJlDL3bFr@cluster0.wz52z8p.mongodb.net/food-del').then(()=>
-        console.log("DB Connected"));
-}
